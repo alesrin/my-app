17aa5c123d020419b9d9aa052d6530d7216ba755
Alesrin <68646511+alesrin@users.noreply.github.com>
2025-11-18 14:05:02 +0100

    Feat: Ejemplo completo de Footer con componentes anidados (estilos básicos)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Header from '../components/Header'
 import Main from '../components/Main'
+import Footer from '../components/Footer'
+import style from '../styles/Home.module.css'
 
 function Home() {
   return (
-    <>
+    <div className={style.home}>
       <Header />
       <Main />
-    </>
+      <Footer />
+    </div>
   )
 }
 
